77d9ddf62e687676c7b127ee39b9c973028ccef1
rohinilokare <rohini@treeni.com>
2019-07-19 16:32:31 +0530

    feature::AllRecipe::fetched recipe directions  steps and preparation time for recipe

--- scrap_database.rb
+++ scrap_database.rb
@@ -10,17 +10,34 @@ class ScrapDatabase
 
 	def initialize
 		@scrap = Scraping.new
-		@db = SQLite3::Database.new 'category.db'
+		@db = SQLite3::Database.new 'AllRecipes.db'
 		categories_table
 	end
 
 	def categories_table
-		@db = SQLite3::Database.new 'category.db'
-		@db.execute("DROP TABLE IF EXISTS category")
-		@db.execute "CREATE TABLE category(Id INTEGER PRIMARY KEY, Name TEXT,  Href TEXT)"
-
+		@db.execute("DROP TABLE IF EXISTS category_table")
+		@db.execute "CREATE TABLE category_table(Id INTEGER PRIMARY KEY, Name TEXT,  Href TEXT)"
 		@scrap.category_hash.each do |key , value|
-				@db.execute("INSERT INTO category(Name, Href) VALUES('#{key}',  '#{value}')")
+				@db.execute("INSERT INTO category_table(Name, Href) VALUES('#{key}',  '#{value}')")
+		end
+		 recipes_table
+		#recipe_ingredients_table
+	end
+
+ 	def recipes_table
+		@db.execute("DROP TABLE IF EXISTS recipes")
+		@db.execute "CREATE TABLE recipes(Id INTEGER PRIMARY KEY, RecipeName TEXT)"
+		@scrap.recipe_name_array.each do |recipe_name|
+			@db.execute("INSERT INTO recipes(RecipeName) VALUES('#{recipe_name}');")
+		end
+	end
+
+	def recipe_ingredients_table
+		# @db = SQLite3::Database.new 'ingredients.db'
+		@db.execute("DROP TABLE IF EXISTS recipe_ingredients")
+		@db.execute "CREATE TABLE recipe_ingredients(Id INTEGER PRIMARY KEY, recipeName TEXT,  ingredients BLOB)"
+		@scrap.ingredients_name_hash.each do |key , value|
+			@db.execute("INSERT INTO recipe_ingredients(recipeName, ingredients) VALUES('#{key}',  '#{value}')")
 		end
 	end
 
--- scraping.rb
+++ scraping.rb
@@ -3,9 +3,8 @@ require 'nokogiri'
 require 'open-uri'
 require 'sqlite3'
 
-
 class Scraping
-	attr_accessor :category_hash
+	attr_accessor :category_hash, :recipe_name_array, :recipe_href_array, :ingredients_name_hash
 
 	def initialize
 		@category_object = Nokogiri::HTML(open('https://www.allrecipes.com/').read)
@@ -20,15 +19,6 @@ class Scraping
 		category_hash_fun
 	end
 
-	def category_href
-		@href_array =Array.new
-		hrefs = @single_div.scan(/href="(.*?)"/)
-		for link in hrefs do
-			@href = link[0].to_s
-			@href_array.push(@href)
-		end
-	end
-
 	def category_name
 		@category_name_array = Array.new
 		category = @single_div.scan(/<span.*>(.*?)<\/span>/)
@@ -38,22 +28,28 @@ class Scraping
 			end
 	end
 
-	def category_hash_fun
-		category_href
-		category_name
-		@category_hash = Hash.new
-		cat_name = 0
-		@href_array.each do |href|
-			@category_hash[@category_name_array[cat_name]] =  href
-			cat_name = cat_name +1
+	def category_link
+		@category_link_array =Array.new
+		hrefs = @single_div.scan(/href="(.*?)"/)
+		for link in hrefs do
+			@href = link[0].to_s
+			@category_link_array.push(@href)
 		end
-		# puts "---------------------------------------------------"
-		# puts "@category_hash: #{@category_hash}"
-		# puts "---------------------------------------------------"
-		 category_view_page
 	end
 
-	def category_view_page
+	def category_hash_fun
+		category_link
+		category_name
+		@category_hash = Hash.new
+		cat_name_index = 0
+		@category_link_array.each do |href|
+			@category_hash[@category_name_array[cat_name_index]] =  href
+			cat_name_index = cat_name_index +1
+		end
+		 category_view_sourse_page
+	end
+
+	def category_view_sourse_page
 		@category_viewpage_array = Array.new
 		for i in @category_name_array
 			html_data =	open(@category_hash[i]).read
@@ -75,7 +71,6 @@ class Scraping
 		recipes_name
 	end
 
-
 	def recipes_name
 		@recipe_name_array = Array.new
 		for section in @category_view__section_array
@@ -89,15 +84,11 @@ class Scraping
 				end
 			end
 		end
-		# puts '------------Recipe Names--------------'
-		# @recipe_name_array.each_with_index do |value, index|
-		# 	puts "#{index}: #{value}"
-		# end
-		recipes_href
+		recipes_link
 	end
 
-	def recipes_href
-		@category_view__div
+	def recipes_link
+		# @category_view__div
 		@recipe_href_array = Array.new
 		for section in @category_view__section_array
 			recipe_hrefs = section.scan(/<h3\s*class=\"fixed-recipe-card__h3\">\s*<a\s*href=\"(.*?)\"/)
@@ -110,57 +101,95 @@ class Scraping
 				count = count + 1
 			end
 		end
-		puts '----------Recipe Link------------------'
-		@recipe_href_array.each_with_index do |value, index|
-			puts "#{index}: #{value}"
-			end
-		 # recipe_hash
-		recipe_ingredients_view
+		recipe_view_sourse_page
 	end
 
-	def recipe_hash
-		recipes_href
-		recipes_name
-		@recipe_hash = Hash.new
-		recipe_name_index = 0
-		@recipe_href_array.each do |href|
-			@recipe_hash[@recipe_name_index_array[recipe_name_index]] =  href
-			recipe_name_index = recipe_name_index +1
-		end
-		puts "-----------------Recipe Hash----------------------------------"
-		@recipe_hash.each do |key, value|
-		puts key + ' : ' + value
-		end
-		puts "---------------------------------------------------"
-	end
-
-	def recipe_ingredients_view
-		@ingredient_view_page_array =Array.new
+	def recipe_view_sourse_page
+		@recipe_view_sourse_page_array =Array.new
 		for i in @recipe_href_array
 			@ingredient_view_page = Nokogiri::HTML(open(i).read)
 			@ingredient_view_page = @ingredient_view_page.to_s
-			@ingredient_view_page_array.push(@ingredient_view_page)
+			@recipe_view_sourse_page_array.push(@ingredient_view_page)
 		end
 		recipe_ingredients_list
 	end
 
 	def recipe_ingredients_list
-		@ingredients_name_array = Hash.new
-		for view_page in @ingredient_view_page_array
+		@ingredients_list_hash = Hash.new
+		for view_page in @recipe_view_sourse_page_array
 			@ingredients_names = view_page.scan(/<li\s*class=\"checkList__line\">.*?\s*<label.*?title=\"(.*?)\"/)
-			# puts @ingredients_names
+			ingredients_array = Array.new
 			for names in @ingredients_names do
-				@name = names[0].to_s
-				# @ingredients_name_array.push(@name)
+				ingredient = names[0].to_s
+				ingredients_array.push(ingredient)
+			end
+			for recname in @recipe_name_array do
+				@ingredients_list_hash[recname] = ingredients_array
 			end
 		end
-		# puts '----------ingredients List------------------'
-		# @ingredients_name_array.each_with_index do |value, index|
-		# puts "#{index}: #{value}"
-		# end
+		# puts @ingredients_list_hash
+		recipe_directions_steps
 	end
+
+	def recipe_directions_steps
+		@recipe_directions_hash = Hash.new
+		for view_page in @recipe_view_sourse_page_array
+			@directions_steps = view_page.scan(/<span\s*class=\"recipe-directions__list--item\">(.*?)\s*<\/span>/)
+			directions_array = Array.new
+			for steps in @directions_steps do
+				step = steps[0].to_s
+				directions_array.push(step)
+			end
+			for recname in @recipe_name_array do
+				@recipe_directions_hash[recname] = directions_array
+			end
+		end
+		# puts @recipe_directions_hash
+		recipe_preparation_time
+	end
+
+		def recipe_preparation_time
+		@recipe_preparation_time_array = Array.new
+			for view_page in @recipe_view_sourse_page_array
+				@preparation_times = view_page.scan(/<li\s*class=\"prepTime__item\"\s*aria-label=\"Prep\s*time:(.*?)\"/)
+				for times in @preparation_times do
+					time = times[0].to_s
+					@recipe_preparation_time_array.push(time)
+				end
+			end
+			puts '----prep time ----------'
+			puts @recipe_preparation_time_array
+			recipe_cook_time
+		end
+
+		def recipe_cook_time
+		@recipe_cook_time_array = Array.new
+			for view_page in @recipe_view_sourse_page_array
+				@cook_times = view_page.scan(/<li\s*class=\"prepTime__item\"\s*aria-label=\"Cook\s*time:(.*?)\"/)
+				for times in @cook_times do
+					time = times[0].to_s
+					@recipe_cook_time_array.push(time)
+				end
+			end
+			puts '--------cook time-----'
+			puts @recipe_cook_time_array
+			recipe_ready_in_time
+		end
+
+		def recipe_ready_in_time
+		@recipe_ready_in_time_array = Array.new
+			for view_page in @recipe_view_sourse_page_array
+				@ready_in_times = view_page.scan(/<li\s*class=\"prepTime__item\"\s*aria-label=\"Ready\s*in(.*?)\"/)
+				for times in @ready_in_times do
+					time = times[0].to_s
+					@recipe_ready_in_time_array.push(time)
+				end
+			end
+			puts '-------ready in---------'
+			puts @recipe_ready_in_time_array
+		end
 
 end
 
-# @scraping = Scraping.new
+@scraping = Scraping.new
 # scrapdata = ScrapDatabase.new
